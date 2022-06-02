f27061b8c09426c00f9583cd221208e287637032
Maxim <vedernikovmaxim@rambler.ru>
2022-06-02 22:20:27 +0300

    Доделана 1 задача

--- task_1.py
+++ task_1.py
@@ -2,19 +2,29 @@
 # Пример: [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д. Порядок элементов менять нельзя
 
 def ascendingList(listUser: list):
-    newList = []
     count = 0
-    while(listUser[count] >= max(listUser)):
+    dictionaryLen = {}
+    dictionaryList = {}
+    while(listUser[count] == max(listUser)):
         count += 1
-    maxm = listUser[count]
-    newList.append(listUser[count])
-    for i in range(count, len(listUser)):
-        if(listUser[i] > maxm):
-            maxm = listUser[i]
-            newList.append(listUser[i])
-    return newList
+    while(count<len(listUser)):
+        newList = []
+        maxMinElement = listUser[count]
+        newList.append(listUser[count])
+        for i in range(count, len(listUser)):
+             if(listUser[i] > maxMinElement):
+                 maxMinElement = listUser[i]
+                 newList.append(listUser[i])
+        dictionaryLen[count] = len(newList)
+        dictionaryList[count] = newList
+        count+=1
+    maxDict = [k for k in dictionaryLen.keys() if dictionaryLen[k] == max(dictionaryLen.values())]
+    endList = dictionaryList[maxDict[0]]
+    return endList
+    
+        
 
 
-listU = [9, 9, 2, 5, 6, 7, 5, 8]
+listU = [9, 9, 5, 2, 3, 7, 5, 8]
 newList = ascendingList(listU)
 print(newList)
