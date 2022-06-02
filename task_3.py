bb209ab3317b80e674f4985d336d9e11e3c2fedd
Maxim <vedernikovmaxim@rambler.ru>
2022-06-02 22:52:37 +0300

    Решена 3 задача

--- task_3.py
+++ task_3.py
@@ -4,12 +4,15 @@
 #является 938-м в списке. Поэтому, имя COLIN получает 938 × 53 = 49714 очков.
 #Какова сумма очков имен в файле?
 
+from itertools import count
+
+
 alphabet = {
- '1': 'A', '2': 'B', '3': 'C', '4': 'D', '5': 'E', '6': 'F',
- '7': 'G', '8': 'H', '9': 'I', '10': 'J', '11': 'K', '12': 'L',
- '13': 'M', '14': 'N', '15': 'O', '16': 'P', '17': 'Q', '18': 'R',
- '19': 'S', '20': 'T', '21': 'U', '22': 'V', '23': 'W', '24': 'X',
- '25': 'Y', '26': 'Z'}
+ 1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F',
+ 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L',
+ 13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R',
+ 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X',
+ 25: 'Y', 26: 'Z'}
 
 def sort(name):
     data = open(name, 'r')
@@ -17,10 +20,26 @@ def sort(name):
     for line in data:
         listName=line.split(',')
     listName.sort()
-    print(listName)
     data.close
-    dataN = open('123.txt', 'a')
+    dataN = open('sort.txt', 'a')
     dataN.writelines(str(listName))
     dataN.close
+    return listName
 
-sort('english_names.txt')
+def point(listNames: list):
+    count = 0
+    for i in range(len(listNames)):
+        bufCount = 0
+        for k in alphabet.keys():
+            for element in listNames[i]:
+                if alphabet[k] == element:
+                     bufCount += k
+        count += bufCount*i
+    return count
+
+
+
+
+list = sort('english_names.txt')
+countList = point(list)
+print(countList)
